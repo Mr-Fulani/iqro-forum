4b21613b5e8e3daba693645a986205ff933380e8
Anvar <anvar.sharipov.1986@gmail.com>
2024-08-02 18:51:44 +0300

    Добавил страницу вывода ошибки прав доступа пользователя, прописал стили

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,15 +128,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
+#
+#
 # ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
 
 
 
